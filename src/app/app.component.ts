f8737c961ae13259cec9fb03a09a38ee61b9a722
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-11-25 18:35:32 -0300

    Adding model Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'Gerenciador de Tarefas';
+  
+  task: Task = new Task(1, "Test task");
+  
+}
+export class Task{
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
 }
