f8288930910fd190ad95322b10e4144cda0e79a5
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-11-26 23:36:38 -0300

    moving the task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+ 
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,6 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Gerenciador de Tarefas';
-  
-  task: Task = new Task(1, "Test task");
-  
+ 
 }
-export class Task{
-  public id: number;
-  public title: string;
 
-  constructor(id: number, title: string){
-    this.id = id;
-    this.title = title;
-    
-  }
-}
