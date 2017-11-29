560096f5b1d64a44ddea5dcd351714106ed3a6e0
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-11-28 21:37:43 -0300

    adding detail to tasks component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -21,11 +21,16 @@ const TASKS: Array<Task> = [
 export class TasksComponent implements OnInit{
     
     public tasks;
+    public selectedTask :Task;
 
     public constructor(){}
 
     public ngOnInit(): void {
       this.tasks = TASKS;  
+    }
+
+    public onSelect(task: Task): void{
+        this.selectedTask = task;
     }
 
 }
