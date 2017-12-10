2e659dc4595054f44cec59623d6465429899e538
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-12-10 11:36:49 -0300

    Adding InMemoryWebApiModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
+//angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+//Angular in memory
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryTaskDataService } from './in-memory-task-data.service';
+
+//Modules
 import { AppRoutingModule } from './app-routing.module'
 
-
+//Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
-import { TaskService } from './tasks/shared/task.service';
 
+//Services
+import { TaskService } from './tasks/shared/task.service';
 
 @NgModule({
   declarations: [
@@ -25,7 +33,8 @@ import { TaskService } from './tasks/shared/task.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryTaskDataService)
   ],
   providers: [ TaskService ],
   bootstrap: [AppComponent]
