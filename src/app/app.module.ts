c139e0edaaf393ced253b8aea8989d2a13a8fc29
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-12-16 14:43:29 -0300

    Adding basic setup to use reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 //angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 //Angular in memory
@@ -49,6 +49,7 @@ import "rxjs/add/observable/throw";
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
     InMemoryWebApiModule.forRoot(InMemoryTaskDataService)
