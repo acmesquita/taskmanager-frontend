8083e92f19953df9f87170769a264a459e344763
Catharina Mesquita <catha.ana.1994@gmail.com>
2017-12-23 17:34:46 -0300

    adding SingInFormComponent to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 const ROUTES = RouterModule.forRoot([
+    { path: 'sign_in', component: SignInFormComponent },
+    { path: 'sign_up', component: SignUpFormComponent },
     { path: 'tasks/:id', component: TaskDetailComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'tasks', component: TasksComponent },
-    { path: 'sign_up', component: SignUpFormComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
   ])
   
